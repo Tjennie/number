26cf0ae53fcd716a8c059094b7f250b250a29cb4
Tjennie <103395335+Tjennie@users.noreply.github.com>
2022-04-19 21:07:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var x = document.getElementById("Text").value;
+    var y = document.getElementById("Texts").value;
+    var z= Number(x) + Number(y);
+    document.getElementById("demo").innerHTML = z ;
+    }
+//Jennie(Feng Xuer)  
+//JavaScript short description: Shows the result of adding two numbers.
